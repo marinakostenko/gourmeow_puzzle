7ad3ea2fe77b83eb3613daae0ed7a1a4ff3874ce
Marina Kostenko <kostenkomarin@gmail.com>
2022-02-19 13:06:25 +0100

    added main page

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => PuzzleBloc(),
+      providers: [
+        BlocProvider(
+          create: (context) => PuzzleBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Gourmeow Puzzle',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+          width: 200,
+          height: 200,
+          margin: EdgeInsets.all(30),
+          child: TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Slide endless_puzzle mode",
+              style: TextStyle(fontSize: 32),
+            ),
           ),
-        ],
-        child: MaterialApp(
-          title: 'Gourmeow Puzzle',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
+        ),
+        Container(
+          width: 200,
+          height: 200,
+          margin: EdgeInsets.all(30),
+          child: TextButton(
+            onPressed: () {
+              Navigator.of(context).push<void>(
+                MaterialPageRoute(
+                  builder: (context) => const PuzzlePage(),
+                ),
+              );
+            },
+            child: const Text(
+              "Endless mode",
+              style: TextStyle(fontSize: 32),
+            ),
           ),
-          home: PuzzlePage(),
-        ),);
+        ),
+      ],
+    );
   }
 }
